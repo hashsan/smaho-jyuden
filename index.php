c92f1234466d25df9bad754c4424f29e4aa9293a
はっしゅさん <149641384+hashsan@users.noreply.github.com>
2024-04-18 15:55:15 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,12 @@ get_header(); // header.phpを呼び出す
 // メインコンテンツの開始
 if (have_posts()):
     while (have_posts()): the_post(); ?>
-        <article>
+        <main>
+        <?php if (!is_front_page()) : ?>
             <h2><?php the_title(); ?></h2>
+        <?php endif; ?>            
             <?php the_content(); ?>
-        </article>
+        </main>
     <?php endwhile;
 endif;
 
